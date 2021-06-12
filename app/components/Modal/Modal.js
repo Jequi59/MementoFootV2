50b97eb69e372689340ab9c09709b56652280e27
DevHÿllen <64634619+Jequi59@users.noreply.github.com>
2021-06-12 22:52:56 +0200

    modal logo update

--- app/components/Modal/Modal.js
+++ app/components/Modal/Modal.js
@@ -7,17 +7,21 @@ function Modal({changeModalState, modalTeam}) {
         <div className="modal-externe" onClick={() => changeModalState(false)} >
             <div className="modal-interne" onClick={(e) => e.stopPropagation()} >
                 <div className="close-btn" onClick={() => changeModalState(false)}>X</div>
-                <div className="modal-flag">
-                    <img src={modalTeam.flag} alt="" />
-                </div>
+                <img className="modal-flag" src={modalTeam.flag} alt={"Drapeau "+modalTeam.name} />
                 <div className="modal-header">
                     {modalTeam.name}
                 </div>
                 <div className="modal-body">
+                    <div className="modal-body-logo">
+                        <img src={modalTeam.logo} alt="" />
+                    </div>
+                    <div className="modal-body-players">
+                        
 
+                    </div>
                 </div>
                 <div className="modal-footer">
-
+                    <p>Ajouter au favori</p>
                 </div>
             </div>
         </div>
