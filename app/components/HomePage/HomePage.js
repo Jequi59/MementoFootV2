566fa2fa2c231495576300c457820a887ab50800
DevHÿllen <64634619+Jequi59@users.noreply.github.com>
2021-06-22 12:40:31 +0200

    ...

--- app/components/HomePage/HomePage.js
+++ app/components/HomePage/HomePage.js
@@ -2,11 +2,34 @@ import React from 'react'
 
 import '../HomePage/HomePage.scss'
 
+import matchs from '../../datas/matchs'
+
 function HomePage(){
+
+    const now = new Date()
+
+    function tranformDate(matchDate){
+        const correctDate = matchDate.split('/');
+        var finalDate = new Date(correctDate[2], correctDate[1] - 1, correctDate[0]);
+        return finalDate    
+    }
+
+    const dateMatch1 = tranformDate(matchs[15].date)
+
+    console.log(now);
+    console.log(dateMatch1);
+
+    if (now > dateMatch1) {
+        console.log("passé");
+    } else {
+        console.log("futur");
+    }
+
+    
     return(
         <div className="home-page">
-                <p>Work in progress...</p>
-                <p>Ici Accueil</p>
+                <h3 className="home-last-matchs-title">Derniers matchs : </h3>
+                <h3 className="home-next-matchs-title">Prochains matchs : </h3>
         </div>
     )
 }
